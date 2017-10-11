2752cfe1ac21602b0f3749ee70180dc700889c00
petercz1 <email.peter.carroll@gmail.com>
2017-10-11 15:59:29 +0800

    There's no trick to being a humorist when you have the whole government working for you. 		-- Will Rogers

--- js/javascript.js
+++ js/javascript.js
@@ -1,24 +1,35 @@
 // AIzaSyC6oCsyUnlTloz-jdSUAn57vMdfVZJnEik
 
 function initMap() {
-   var uluru = {lat: -25.363, lng: 131.044};
-   var map = new google.maps.Map(document.getElementById('map'), {
-     zoom: 4,
-     center: uluru
-   });
-   var marker = new google.maps.Marker({
-     position: uluru,
-     map: map
-   });
- }
+  var uluru = {
+    lat: -25.363,
+    lng: 131.044
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
 
- $(document).ready(setup);
+$(document).ready(setup);
 
- function setup(){
-     console.log('Inside setup');
+function setup() {
+  console.log('Inside setup');
+  $('#send').click(send_data);
+}
 
- }
+function send_data(){
+    console.log('Inside send_data');
+    var name = $('#name').val();
+    var name = $('#name').val();
+    var name = $('#name').val();
+}
 
 
 
- 
+
+
