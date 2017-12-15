9569b8f9ffa2afaa960f06a1dfb3197447c24441
petercz1 <email.peter.carroll@gmail.com>
2017-12-15 14:50:06 +0800

    This fortune is dedicated to your mother, without whose invaluable assistance last night would never have been possible.

--- results.php
+++ results.php
@@ -1,3 +1,6 @@
 <?php
+$path = $_SERVER["SCRIPT_FILENAME"];
 
-echo 'whoop - it works...'
+file_put_contents("php://stdout", "\nRequested: $path");
+error_log('testing', 3, 'logger.log');
+echo "<p>Hello World</p>";
